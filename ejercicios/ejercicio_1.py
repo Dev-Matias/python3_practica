729e1e11d7e7f3a40b17f33b05c590546be6f422
Dev-Matias <devmatias01@gmail.com>
2025-05-05 20:55:05 -0300

    resulto eje 1

---
+++ ejercicios/ejercicio_1.py
@@ -0,0 +1,6 @@
+comida_favorita = input("¿Cuál es tu comida favorita? ")
+comida_favorita = comida_favorita.upperPizza()
+precio_comida = float(input("¿Cuánto cuesta tu comida favorita? "))
+precio_comida = round(precio_comida, 2)
+
+print(f"Tu comida favorita es {comida_favorita} y cuesta {precio_comida} papiros.")
