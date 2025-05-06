3729f0e681d0b59397b52974226196931049d3b0
Dev-Matias <devmatias01@gmail.com>
2025-05-05 21:27:13 -0300

    resuelto ej 3

---
+++ ejercicios/ejercicio_2.py
@@ -0,0 +1,9 @@
+# Ejercicio 2: Calcular el área de un círculo
+#(fórmula: `π * radio²`). Usa `radio = 5` y `π = 3.1416`.
+
+# Definición de variables
+PI = 3.1416
+radio = 5
+area = PI * radio ** 2
+# Imprimir el resultado
+print(f"El área del círculo con radio {radio} es: {area}")
